1386cdfb595ebb91deafeff491f238a5093e5a6e
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-13 15:03:21 +0100

    ADD > font awesome npm packages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// importa icone specifiche
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+//aggiungo alla libreria
+library.add(faStar)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
