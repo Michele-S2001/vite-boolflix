b49dd4c2d462928fa1ce555bbb74bbd882d77c40
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-13 12:14:02 +0100

    ADD > milestone 3 (part 1) added thumbnail images

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,7 +5,8 @@ export default {
 
   data() {
     return {
-      componentName: 'AppCard'
+      componentName: 'AppCard',
+      itemThumbnail: ''
     }
   },
 
@@ -16,11 +17,19 @@ export default {
     }
   },
 
+  methods: {
+    fullThumbURL() {
+      // this.itemThumbnail = "`https://image.tmdb.org/t/p/w342${this.item.backdrop_path}`";
+      this.itemThumbnail = "ciao"
+      return this.itemThumbnail;
+    }
+  },
+
   computed: {
     flagPath() {
       return store.flags[this.item.original_language]
     },
-  }
+  },
 }
 
 </script>
@@ -28,21 +37,29 @@ export default {
 <template>
   <!-- film or tv card -->
   <div class="card">
-    <ul class="card__movie-details">
-      <li class="movie-detail">
+    <ul class="card__item-details">
+      <li class="item-detail">
         <span>Titolo:</span> {{ item.title || item.name }}
       </li>
-      <li class="movie-detail">
+      <li class="item-detail">
         <span>Titolo originale:</span> {{ item.original_title || item.original_name }}
       </li>
-      <li class="movie-detail">
+      <li class="item-detail">
         <img class="flag" v-if="flagPath" :src="flagPath" alt="country flag">
         <p v-else>Lingua: {{ item.original_language }}</p> 
       </li>
-      <li class="movie-detail">
+      <li class="item-detail">
         <span>Voto:</span> {{ item.vote_average }}
       </li>
+      <li class="item-detail">
+        <img 
+          :src="`https://image.tmdb.org/t/p/w300${this.item.backdrop_path}`"
+          alt="thumbnail"
+        >
+      </li>
     </ul>
+
+    
   </div>
 
 </template>
@@ -58,8 +75,8 @@ export default {
     color: $white;
     font-size: 16px;
 
-    &__movie-details {
-      .movie-detail  {
+    &__item-details {
+      .item-detail  {
         margin-bottom: 10px;
         & span {
           font-weight: bold;
